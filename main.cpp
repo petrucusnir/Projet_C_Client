86c3fe153217c1230f2505dd18886d6524c60da8
petrucusnir <petru.cusnir@depinfonancy.net>
2017-01-17 16:52:18 +0100

    il faudra peutetre rajouter le serveur comme il trouve pas import sockets ?

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "mainwindow.h"
+#include <unistd.h>
+#include <QDebug>
+#include "clienttcp.h"
+#include <QInputDialog>
+#include <QDir>
+
+using namespace Phonon;
 
 int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+{
+    srand(time(NULL));
+
+    QApplication app(argc, argv);
+
+    ClientTcp c1;
+    QString ip = "192.168.43.98";
+    c1.recoit_IP(ip);
+    QWidget showWid;
+    showWid.setGeometry(10,10,200,200);
+    showWid.show();
+    while(true)
+    {
+        bool ok;
+        QString text = QInputDialog::getText(&showWid, "Hello", "enter text", QLineEdit::Normal,
+                              QDir::home().dirName(), &ok);
+        if (ok && !text.isEmpty())
+        {
+            c1.recoit_texte(text);
+        }
+    }
+
+    return app.exec();
+
 }
